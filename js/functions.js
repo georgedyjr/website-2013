59a1a472a64100e87fc4cc9fac627fe9a12c18e3
Roy McKenzie <roymckenzie@gmail.com>
2013-04-24 18:41:36 -0700

    Initial commit

---
+++ js/functions.js
@@ -0,0 +1,54 @@
+$(document).on('ready', function () {
+
+	// For parallax effect on section and body backgrounds
+	//$('[data-type="background"]').each( function () {
+		//var $bgElement = $(this); // assigning the object
+
+		//$(window).on('scroll', function () {
+			//var yPos = -($(window).scrollTop() / $bgElement.data('speed')); 
+
+			// Put together our final background position
+			//var coords = '50% '+ yPos + 'px';
+
+			// Move the background
+			//$bgElement.css({ backgroundPosition: coords });
+		//})
+	//});
+
+}); 
+
+// Click handlers for nav
+$('#header-site a').on('click', function (e) {
+
+	extraOffset = 0;
+	linkId = $(this).attr('href');
+
+	if (linkId == '#services') { extraOffset = -145}
+
+	if ($(window).scrollTop() > 10) {
+		scrollPage($(linkId).offset().top - extraOffset);
+	} else {
+		scrollPage($(linkId).offset().top - (265 + extraOffset));
+	}
+	e.preventDefault();
+
+});
+
+function scrollPage(scrollOffset) {
+	$('html, body').animate({
+		scrollTop: scrollOffset
+	}, 1000);
+}
+
+// Set fixed header
+$(window).on('scroll', function () {
+	if ( $(window).width() > 500) {
+		if ($(this).scrollTop() > 10) {
+			$('#header-site').addClass('header-persist');
+			$('#about').addClass('darken');
+		} else {
+			$('#header-site').removeClass('header-persist');		
+			$('#about').removeClass('darken');
+		}
+	}
+});
